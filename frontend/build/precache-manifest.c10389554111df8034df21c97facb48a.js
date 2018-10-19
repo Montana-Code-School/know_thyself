431abc8917c8b1ecc0ecb63f151cd33f03109d9c
Alison Morgan <alison.morgan406@gmail.com>
2018-10-19 13:48:12 -0600

    adding build to source

---
+++ frontend/build/precache-manifest.c10389554111df8034df21c97facb48a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4c46e25e4a832fd759de",
+    "url": "/static/js/main.4c46e25e.chunk.js"
+  },
+  {
+    "revision": "3a5149794fe228990408",
+    "url": "/static/js/1.3a514979.chunk.js"
+  },
+  {
+    "revision": "4c46e25e4a832fd759de",
+    "url": "/static/css/main.9c628e9e.chunk.css"
+  },
+  {
+    "revision": "357d6f27e4dfc6b2b69245b8c6d7af1a",
+    "url": "/index.html"
+  }
+];
