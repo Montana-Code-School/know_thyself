da25dc943f774a9d5a851fa1cd1ded36f4addb02
Alison Morgan <alison.morgan406@gmail.com>
2018-11-07 15:42:07 -0700

    not an empty commit message

--- frontend/src/parser/HTMLparser.js
+++ frontend/src/parser/HTMLparser.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactHtmlParser from 'react-html-parser';
-
+//this is being used
 class HtmlComponent extends Component {
   render() {
     return <div>{ ReactHtmlParser(this.props.entry) }</div>
